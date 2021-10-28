0924ef8792b94ffefafc60fd59e3a65f2c5feeb1
m1raynee <pechenko-7@ya.ru>
2021-10-28 11:40:33 +0300

    shity implementation

--- cogs/meta.py
+++ cogs/meta.py
@@ -3,28 +3,28 @@ import unicodedata
 from disnake.ext import commands
 import disnake
 
+
+def to_string(c, *, skip_url=False):
+    digit = f'{ord(c):x}'
+    name = unicodedata.name(c, 'Name not found.')
+    r = f'`\\U{digit:>08}`: {name} - {c} \N{EM DASH}'
+    if skip_url:
+        r += f' <http://www.fileformat.info/info/unicode/char/{digit}>'
+    return r
+
+async def charinfo_autocomp(inter, value):
+    if len(value) > 25:
+        return {'Only up to 25 characters at a time.': ''}
+    return {
+        to_string(c, skip_url=True): value
+        for c in value
+    }
+
 class Meta(commands.Cog):
     """Meta"""
 
     def __init__(self, bot):
         self.bot: commands.Bot = bot
-    
-    @staticmethod
-    def to_string(c, *, skip_url=False):
-        digit = f'{ord(c):x}'
-        name = unicodedata.name(c, 'Name not found.')
-        r = f'`\\U{digit:>08}`: {name} - {c} \N{EM DASH}'
-        if skip_url:
-            r += f' <http://www.fileformat.info/info/unicode/char/{digit}>'
-        return r
-
-    async def charinfo_autocomp(self, inter, value):
-        if len(value) > 25:
-            return {'Only up to 25 characters at a time.': ''}
-        return {
-            self.to_string(c, skip_url=True): value
-            for c in value
-        }
     
     @commands.slash_command()
     async def charinfo(self, inter: disnake.ApplicationCommandInteraction,
@@ -39,10 +39,11 @@ class Meta(commands.Cog):
         if not len(characters):
             return inter.response.send_message('Nothing to display.', ephemeral=True)
 
-        msg = '\n'.join(map(self.to_string, characters))
+        msg = '\n'.join(map(to_string, characters))
         if len(msg) > 2000:
             return await inter.response.send_message('Output too long to display.', ephemeral=True)
         await inter.response.send_message(msg, ephemeral=True)
+        
 
 def setup(bot):
     bot.add_cog(Meta(bot))
